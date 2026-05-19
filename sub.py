9bab3e99abb1c1bde6d070fb031d24a18e74609a
Empty465 <normal0308@gmail.com>
2026-05-19 22:30:51 +0900

    sub.py의 주석 수정 COMMIT_EDITMSG창이 뜨면 첫 줄은 제목이고 다음 줄 부터는 자세한 내용이라서 제목과는 다르게 글자 수 제한이 없다. 이걸 왜 이제 안 것일까.

--- sub.py
+++ sub.py
@@ -1,8 +1,6 @@
 def sub(nums):  #매개변수 nums의 원소들을 빼는 함수 (str 형태의 리스트를 입력받아야 함)
-    if("." in nums[0]):  #첫 번째 원소에 .(소수점)이 있다면
-        total_nums = float(nums[0])  #최종값 정의 (첫 번째 원소를 실수형으로 변환)
-    else:
-        total_nums = int(nums[0])  #최종값 정의 (첫 번째 원소를 정수형으로 변환)
+    if("." in nums[0]): total_nums = float(nums[0])  #첫번째 num 원소가 소수점이 있으면, 첫 수(최종값)를 실수형으로 변형뒤, 값 대입
+    else: total_nums = int(nums[0])  #아니면 정수값 대입
 
     for n in nums[1:]:  #nums 의 각각의 원소들을 순서대로 n에 넣어서 실행 (첫 번째 원소 제외)
         if("." in n): total_nums -= float(n)    #n에 .(소수점)이 있다면 n을 실수형으로 변환한 뒤에 total_nums에서 빼기
