46b68509adb4968cdf00061bd46c82702a1dfb79
Empty465 <normal0308@gmail.com>
2026-05-19 12:10:58 +0900

    실수값도 계산할 수 있으며, 입력값이 실수면 실수 (소수점 포함) 포함, 정수면 정수 (소수점 제외) 출력

--- sub.py
+++ sub.py
@@ -1,2 +1,3 @@
 def sub(a, b):  #매개변수 a와 b를 빼는 함수
-    return a - b    #a-b 출력(리턴)
+    if("." in a or "." in b): return float(a) - float(b)    #a또는 b에 .(소수점)이 있다면 둘을 실수형으로 변환한 뒤에 뺀 값을 리턴
+    else: return int(a) - int(b)    #아니라면 정수형으로 변환하여 뺀 값을 리턴
--- sum.py
+++ sum.py
@@ -1,2 +1,3 @@
 def add(a, b):  #매개변수 a와 b를 더하는 함수
-    return a + b    #a+b 출력(리턴)
+    if("." in a or "." in b): return float(a) + float(b)    #a또는 b에 .(소수점)이 있다면 둘을 실수형으로 변환한 뒤에 더한 값을 리턴
+    else: return int(a) + int(b)    #아니라면 정수형으로 변환하여 더한 값을 리턴
