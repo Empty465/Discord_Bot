e4bd68751981e5c0fdcdef0932bf42a688a57cdf
Empty465 <normal0308@gmail.com>
2026-05-19 12:34:51 +0900

    더하기, 빼기 명령어에 입력 가능 숫자 제한 해제 (원하는 수만큼 입력 가능)

--- sub.py
+++ sub.py
@@ -1,3 +1,10 @@
-def sub(a, b):  #매개변수 a와 b를 빼는 함수
-    if("." in a or "." in b): return float(a) - float(b)    #a또는 b에 .(소수점)이 있다면 둘을 실수형으로 변환한 뒤에 뺀 값을 리턴
-    else: return int(a) - int(b)    #아니라면 정수형으로 변환하여 뺀 값을 리턴
+def sub(nums):  #매개변수 nums의 원소들을 빼는 함수
+    if("." in nums[0]):  #첫 번째 원소에 .(소수점)이 있다면
+        total_nums = float(nums[0])  #최종값 정의 (첫 번째 원소를 실수형으로 변환)
+    else:
+        total_nums = int(nums[0])  #최종값 정의 (첫 번째 원소를 정수형으로 변환)
+
+    for n in nums[1:]:  #nums 의 각각의 원소들을 순서대로 n에 넣어서 실행 (첫 번째 원소 제외)
+        if("." in n): total_nums -= float(n)    #n에 .(소수점)이 있다면 n을 실수형으로 변환한 뒤에 total_nums에서 빼기
+        else: total_nums -= int(n)    #아니라면 n을 정수형으로 변환하여 total_nums에서 빼기
+    return total_nums    #total_nums 출력(리턴)
--- sum.py
+++ sum.py
@@ -1,3 +1,6 @@
-def add(a, b):  #매개변수 a와 b를 더하는 함수
-    if("." in a or "." in b): return float(a) + float(b)    #a또는 b에 .(소수점)이 있다면 둘을 실수형으로 변환한 뒤에 더한 값을 리턴
-    else: return int(a) + int(b)    #아니라면 정수형으로 변환하여 더한 값을 리턴
+def add(nums):  #매개변수 nums의 원소들을 더하는 함수
+    total_nums = 0  #최종값 정의
+    for n in nums:  #nums 의 각각의 원소들을 순서대로 n에 넣어서 실행
+        if("." in n): total_nums += float(n)    #n에 .(소수점)이 있다면 n을 실수형으로 변환한 뒤에 total_nums에 더하기
+        else: total_nums += int(n)    #아니라면 n을 정수형으로 변환하여 total_nums에 더하기
+    return total_nums    #total_nums 출력(리턴)
